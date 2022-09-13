41c5fab28c6aab9010731816c6628a8b8a263b4b
Kaidan <kaidangustave@yahoo.com>
2022-09-13 13:54:14 -0500

    more work on API + added new util.js file

--- assets/js/nws-api.js
+++ assets/js/nws-api.js
@@ -1,4 +1,4 @@
-let nwsBaseURL = 'https://api.weather.gov';
+let nwsBaseURL = 'https://api.weather.gov/';
 let forecastRoute = 'points/';
 
 // formats lat and lon into full forecast API URL
@@ -6,5 +6,12 @@ let forecastURL = (lat, lon) => `${nwsBaseURL}${forecastRoute}${lat},${lon}`;
 
 function fetchForecast(lat, lon) {
     let url = forecastURL(lat, lon);
-    fetch(url).then((response) => {})
+    fetch(url).then(responseToJSON)
+        .then((json) => {
+            console.log(json);
+            return fetch(json.properties.forecast).then(responseToJSON);
+        })
+        .then((json) => {
+            console.log(json);
+        })
 }
