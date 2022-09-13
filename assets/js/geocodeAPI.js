015eb9f7725c07cd36b3354fcf5026aec8f7dd59
park-d <54589582+park-d@users.noreply.github.com>
2022-09-13 14:27:00 -0500

    function to grab searched city information from openstreetmap API

---
+++ assets/js/geocodeAPI.js
@@ -0,0 +1,31 @@
+// let searchBtn = $('#search-btn');
+// let searchQuery = $('#search-city')
+// let searchedCity = searchBtn.val()
+
+let apiBase = 'https://nominatim.openstreetmap.org/search?';
+
+function cityToLatLon(cityName) {
+    let requestURL = apiBase + `city=${cityName}&format=json`;
+
+    return fetch(requestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data[0].display_name);
+            let geocodedArray = {
+                city: data[0].display_name.split(',')[0],
+                state: data[0].display_name.split(',')[2],
+                country: data[0].display_name.split(',')[3],
+                lat: data[0].lat,
+                lon: data[0].lon
+            };
+            console.log(geocodedArray);
+            return geocodedArray;
+        });
+}
+
+cityToLatLon('Chicago').then(function (data) {
+    console.log(data.lat);
+    console.log(data.lon);
+});
