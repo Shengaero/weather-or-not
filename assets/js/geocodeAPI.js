9baba22e2821b5476912dcb03afd28ae5311d9fc
Kaidan <kaidangustave@yahoo.com>
2022-09-14 11:52:54 -0500

    fixed some small bugs with api functions

--- assets/js/geocodeAPI.js
+++ assets/js/geocodeAPI.js
@@ -1,19 +1,18 @@
 let apiBase = 'https://nominatim.openstreetmap.org/search?';
 
-function cityToLatLon(cityName) {
+function fetchLatLon(cityName) {
     let requestURL = apiBase + `city=${cityName}&format=json`;
 
     return fetch(requestURL)
-        .then(function (response) {
-            return response.json();
-        })
+        .then(responseToJSON)
         .then(function (data) {
             // output from open street map for name gives "city(0), county(1), state(2), country(3)" so I split on commas to separate locations
             let splitArray = data[0].display_name.split(',');
             let geocodedArray = {
                 city: splitArray[0],
-                state: splitArray[2],
-                country: splitArray[3],
+                displayName: data[0].display_name,
+                // state: splitArray[2],
+                // country: splitArray[3],
                 lat: data[0].lat,
                 lon: data[0].lon
             };
