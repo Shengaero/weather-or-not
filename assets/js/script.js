5003fd177bfd1d854bb1eb3ea199540b6b5f0fd4
park-d <54589582+park-d@users.noreply.github.com>
2022-09-14 13:30:24 -0500

    added barebones event listener for submitting city

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,12 @@
 let searchBtn = $('#search-btn');
 let locationInput = $("#search-city");
 let displayInfo = $("#tools-needed");
+
+//click event for submiting a searched city
+searchBtn.click(function (event) {
+    event.preventDefault();
+    // storing the user input in searchCity variable
+    let searchCity = locationInput.val();
+    //changing the href to include a query variables
+    location.search = `q=${searchCity}`;
+});
